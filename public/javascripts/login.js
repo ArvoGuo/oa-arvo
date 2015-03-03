d75885d88fd7514ca1d07a3debc8ef6733a54e57
Arvo <ArvoGuo@gmail.com>
2015-03-03 15:31:01 +0800

    add css3 , readme

---
+++ public/javascripts/login.js
@@ -0,0 +1,7 @@
+
+$(document).ready(function(){
+  $('.footer').on('click', function() {
+    $('.content').toggleClass('register');
+  });
+
+});
--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,25 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  console.log(44123);
-  next()
-}, function(req,res){
-  res.render('index', {title: "dddd"});
+router.get('/', function(req, res) {
+  res.redirect('/login');
 });
+router.get('/test', function(req, res) {
+  res.render('test', {message: "test"});
+});
+router.get('/login', function(req, res) {
+  res.render('login');
+});
+router.get('/index', function(req, res) {
+  res.render('index');
+});
+router.post('/login/login', function(req, res) {
+  // res.send({
+  //   code: '200',
+  //   message: 'login success'
+  // });
+  res.redirect('/index');
+});
+
 
 module.exports = router;
